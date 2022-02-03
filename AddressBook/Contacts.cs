3802129d5ae30d79bd2b3fd1c4efdaa16e7b96dc
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-04 00:29:51 +0530

    [Nikhil Kumar]:Contacts Created

---
+++ AddressBook/Contacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Addressess { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string email { get; set; }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,6 +6,21 @@ namespace AddressBook
         public static void Main()
         {
             Console.WriteLine("Welcome to Address Book System Program");
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter First Name: ");
+            contacts.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name: ");
+            contacts.LastName = Console.ReadLine();
+            Console.Write("Enter Address : ");
+            contacts.Addressess = Console.ReadLine();
+            Console.Write("Enter City : ");
+            contacts.City = Console.ReadLine();
+            Console.Write("Enter State : ");
+            contacts.State = Console.ReadLine();
+            Console.Write("Enter ZipCode: ");
+            contacts.ZipCode = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            contacts.PhoneNumber = Console.ReadLine();
 
         }
     }
