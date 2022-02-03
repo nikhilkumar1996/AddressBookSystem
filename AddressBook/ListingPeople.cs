97d994af4038f87afa837a303aaca5b2917604b0
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-04 01:17:29 +0530

    [Nikhil Kumar]:Contacts are Added

---
+++ AddressBook/ListingPeople.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class ListingPeople
+    {
+        List<Contacts> addressBook=new List<Contacts>();
+        public void CreateContacts()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter First Name: ");
+            contacts.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name: ");
+            contacts.LastName = Console.ReadLine();
+            Console.Write("Enter Address : ");
+            contacts.Addressess = Console.ReadLine();
+            Console.Write("Enter City : ");
+            contacts.City = Console.ReadLine();
+            Console.Write("Enter State : ");
+            contacts.State = Console.ReadLine();
+            Console.Write("Enter ZipCode: ");
+            contacts.ZipCode = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            contacts.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Contacts Created:" + contacts.FirstName + "\n" + contacts.LastName + "\n" + contacts.Addressess + "\n" + contacts.City + "\n" + contacts.State + "\n" + contacts.ZipCode + "\n" + contacts.PhoneNumber);
+            addressBook.Add(contacts);
+            
+        }
+            
+    }
+}
