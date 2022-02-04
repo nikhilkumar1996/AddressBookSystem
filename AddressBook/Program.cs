d574397549d13c78da414de40b1bd4f8d98a1de3
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-04 21:47:21 +0530

    [Nikhil Kumar]:Multiple Contacts Created

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,13 +5,22 @@ namespace AddressBook
     {
         public static void Main()
         {
-            
 
+            Console.WriteLine("Welcome To Address Book System");
             ListingPeople LP=new ListingPeople();
-            LP.CreateContacts();
+            Console.WriteLine("Enter How many Contacts you want to Add");
+            int num=Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <=num; i++)
+            {
+                Console.WriteLine("Create a Contact");
+                LP.CreateContacts();
+            }
+
+            LP.Display();
             LP.EditContact();
             LP.Display();
             LP.DeleteContact();
+            LP.Display();
 
 
 
